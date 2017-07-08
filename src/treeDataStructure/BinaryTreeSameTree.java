6bc8fd719068657f0a96c13f6ca69891cf346c47
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-07-08 20:36:40 +0200

    Tree Data Structure initial commit

---
+++ src/treeDataStructure/BinaryTreeSameTree.java
@@ -0,0 +1,34 @@
+package treeDataStructure;
+
+/**
+ * Created by Mati on 08.07.2017.
+ */
+public class BinaryTreeSameTree {
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        node1.addChildren(new Node(2), new Node(3));
+        Node node2 = new Node(1);
+        node2.addChildren(new Node(2), new Node(4));
+        boolean result = sameTree(node1, node2);
+        System.out.println("Result: " + result);
+    }
+
+    public static boolean sameTree(Node head1, Node head2) {
+        if (head1 == null && head2 == null) {
+            return true;
+        }
+
+        if (head1 == null) {
+            return false;
+        } else if (head2 == null) {
+            return false;
+        }
+
+        if (sameTree(head1.getLeft(), head2.getLeft()) && sameTree(head1.getRight(), head2.getRight())) {
+            return head1.getId() == head2.getId();
+        }
+        return false;
+    }
+
+}
