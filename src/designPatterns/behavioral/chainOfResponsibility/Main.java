c32662073f109c7e079940432735ae26b48ae299
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-07-09 20:42:28 +0200

    Adding Chain of Responsibility implementation example

---
+++ src/designPatterns/behavioral/chainOfResponsibility/Main.java
@@ -0,0 +1,39 @@
+package designPatterns.behavioral.chainOfResponsibility;
+
+import java.io.IOException;
+
+/**
+ * Created by Mati on 09.07.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int randomNumber = (int) Math.ceil(Math.random() * 2);
+        methodThree(randomNumber);
+    }
+
+    public static void methodOne(int randomNumber) throws IOException, NullPointerException {
+        if (randomNumber == 1) {
+            throw new IOException("IO Exception");
+        } else if (randomNumber == 2) {
+            throw new NullPointerException("Null Pointer Exception");
+        }
+    }
+
+    public static void methodTwo(int randomNumber) throws IOException {
+        try {
+            methodOne(randomNumber);
+        } catch (NullPointerException ex) {
+            System.out.println("Catch Null Pointer Exception inside method two");
+        }
+    }
+
+    public static void methodThree(int randomNumber) {
+        try {
+            methodTwo(randomNumber);
+        } catch (IOException e) {
+            System.out.println("Catch IO Exception Exception inside method three");
+        }
+    }
+
+}
