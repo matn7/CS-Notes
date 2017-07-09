048eef94648abb7ff73d0b109e075dc7df773fc9
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-07-09 14:44:57 +0200

    Adding Strategy Pattern implementation example

---
+++ src/designPatterns/behavioral/strategy/Main.java
@@ -0,0 +1,42 @@
+package designPatterns.behavioral.strategy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by Mati on 09.07.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Step1: Create list of strings
+        List<String> listOfStrings = new ArrayList<>();
+
+        // Step2: Populate listOfStrings
+        listOfStrings.add("Pies");
+        listOfStrings.add("Slon");
+        listOfStrings.add("Panda");
+        listOfStrings.add("Biedronka");
+
+        System.out.println("Before sorting: " + listOfStrings);
+
+        // Step3: Sort list of strings. But when Panda on the first position on the list
+        Collections.sort(listOfStrings, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1.equals("Panda") && !s2.equals("Panda")) {
+                    return -1;
+                } else if (s2.equals("Panda") && !s1.equals("Panda")) {
+                    return 1;
+                }
+                return s1.compareTo(s2);
+            }
+        });
+
+        System.out.println("After Sorting: " + listOfStrings);
+    }
+
+}
