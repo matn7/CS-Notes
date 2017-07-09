1a38990498dd9b30b707e7f28e30c892165e16c6
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-07-09 16:54:04 +0200

    Adding example of Singleton Patterns

---
+++ src/designPatterns/creational/singleton/SingletonEnum.java
@@ -0,0 +1,15 @@
+package designPatterns.creational.singleton;
+
+/**
+ * Created by Mati on 09.07.2017.
+ */
+public enum SingletonEnum {
+
+    INSTANCE;
+
+    public void doSomething() {
+
+    }
+
+    // use: SingletonEnum singleton = SingletonEnu.INSTANCE;
+}
