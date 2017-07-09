a63a4a882cb0d653c03280b705c9c2263fb37b14
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-07-09 17:07:50 +0200

    Adding Decorator Pattern implementation example

---
+++ src/designPatterns/structural/decorator/Main.java
@@ -0,0 +1,17 @@
+package designPatterns.structural.decorator;
+
+/**
+ * Created by Mati on 09.07.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Order fourSeasonPizza = new Pizza("Four season", 10);
+        fourSeasonPizza = new RegularExtra("Pepperoni", 4, fourSeasonPizza);
+        fourSeasonPizza = new DoubleExtra("Mozarella", 2, fourSeasonPizza);
+        fourSeasonPizza = new NoCostExtra("becon", 2, fourSeasonPizza);
+
+        System.out.println(fourSeasonPizza.getPrice() + " : " + fourSeasonPizza.getLabel());
+    }
+
+}
---
+++ src/designPatterns/structural/decorator/Order.java
@@ -0,0 +1,11 @@
+package designPatterns.structural.decorator;
+
+/**
+ * Created by Mati on 09.07.2017.
+ */
+public interface Order {
+
+    double getPrice();
+    String getLabel();
+
+}
