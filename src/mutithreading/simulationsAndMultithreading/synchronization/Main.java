88012d03eb314f78bb07a94b5d821630c8890f79
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-07-10 21:44:02 +0200

    Simulation and Multithreading synchronization, initial commit

---
+++ src/mutithreading/simulationsAndMultithreading/synchronization/Main.java
@@ -0,0 +1,41 @@
+package mutithreading.simulationsAndMultithreading.synchronization;
+
+/**
+ * Created by Mati on 10.07.2017.
+ */
+public class Main {
+
+    private static int count = 0;
+    private static synchronized void increment() {
+        count++;
+    }
+
+    public static void process() {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 200; i++) {
+                increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 200; i++) {
+                increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        process();
+        System.out.println("Count: " + count);
+    }
+
+}
