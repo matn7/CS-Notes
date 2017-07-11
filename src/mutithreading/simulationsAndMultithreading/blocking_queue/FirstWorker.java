118404a58130d1fc9176760b4bf961cc8d778b5b
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-07-11 21:59:27 +0200

    Blocking queue

---
+++ src/mutithreading/simulationsAndMultithreading/blocking_queue/FirstWorker.java
@@ -0,0 +1,30 @@
+package mutithreading.simulationsAndMultithreading.blocking_queue;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Created by Mati on 11.07.2017.
+ */
+public class FirstWorker implements Runnable {
+
+    private BlockingQueue<String> blockingQueue;
+
+    public FirstWorker(BlockingQueue<String> blockingQueue) {
+        this.blockingQueue = blockingQueue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("First Worker");
+                blockingQueue.put("A");
+                Thread.sleep(1000);
+                System.out.println();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
