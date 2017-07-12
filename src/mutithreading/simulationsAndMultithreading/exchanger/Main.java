2ce2f59de76031f49949c260afb69b85f6153129
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-07-12 21:04:49 +0200

    Adding Exchanger

---
+++ src/mutithreading/simulationsAndMultithreading/exchanger/Main.java
@@ -0,0 +1,20 @@
+package mutithreading.simulationsAndMultithreading.exchanger;
+
+import java.util.List;
+import java.util.concurrent.Exchanger;
+
+/**
+ * Created by Mati on 12.07.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Exchanger<List<Integer>> exchanger = new Exchanger<>();
+        Thread t1 = new Thread(new FillingWorker(exchanger));
+
+        t1.start();
+
+
+    }
+
+}
