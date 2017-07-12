df683d4c1a547b7f8486fe9cbcaf74140a4b8ea9
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-07-12 19:00:34 +0200

    Adding factorial implementation iterative and recursive case

---
+++ src/other_algorithms/factorial/Iterative.java
@@ -0,0 +1,24 @@
+package other_algorithms.factorial;
+
+/**
+ * Created by Mati on 12.07.2017.
+ */
+public class Iterative {
+
+    public static void main(String[] args) {
+        System.out.println(factorial(0));
+    }
+
+    public static int factorial(int num) {
+        int result = 1;
+        if (num == 0) {
+            return 1;
+        } else {
+            for (int i = 1; i <= num; i++) {
+                result *= i;
+            }
+            return result;
+        }
+    }
+
+}
