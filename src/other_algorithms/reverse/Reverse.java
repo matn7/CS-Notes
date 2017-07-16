a209494d31fb0e6c216df1afcf8e086b66c592c7
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-07-16 17:53:55 +0200

    Armstrong, reverse, prime algorithms implementation

---
+++ src/other_algorithms/reverse/Reverse.java
@@ -0,0 +1,23 @@
+package other_algorithms.reverse;
+
+/**
+ * Created by Mati on 16.07.2017.
+ */
+public class Reverse {
+    // Program to reverse string. Example China will be anihC
+
+    public static void main(String[] args) {
+        System.out.print(reverse("China"));
+    }
+
+    public static String reverse(String str) {
+        char[] strChar = str.toCharArray();
+
+        for (int i = 0; i < strChar.length; i++) {
+            strChar[i] = str.charAt(str.length() - i - 1);
+        }
+        return String.valueOf(strChar);
+
+    }
+
+}
