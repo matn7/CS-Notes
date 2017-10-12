07e8cb64750ae398bc798e62e2b7925bcd35ff29
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-10-12 20:37:48 +0200

    Data structures

---
+++ src/main/java/data_structures/stack/Stack2.java
@@ -0,0 +1,38 @@
+package data_structures.stack;
+
+/**
+ * Created by Mati on 05.10.2017.
+ */
+public class Stack2 {
+
+    private static class Node {
+        private int data;
+        private Node next;
+        private Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private Node top;
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int peek() {
+        return top.data;
+    }
+
+    public void push(int data) {
+        Node node = new Node(data);
+        node.next = top;
+        top = node;
+    }
+
+    public int pop() {
+        int data = top.data;
+        top = top.next;
+        return data;
+    }
+
+}
---
+++ src/main/java/data_structures/tree/BinarySearchTree.java
@@ -0,0 +1,63 @@
+package data_structures.tree;
+
+/**
+ * Created by Mati on 05.10.2017.
+ */
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public void insert(int value) {
+        if (value <= data) {
+            if (left == null) {
+                left = new Node(value);
+            } else {
+                left.insert(value);
+            }
+        } else {
+            if (right == null) {
+                right = new Node(value);
+            } else {
+                right.insert(value);
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if (value == data) {
+            return true;
+        } else if (value < data) {
+            if (left == null) {
+                return  false;
+            } else {
+                return left.contains(value);
+            }
+        } else {
+            if (right == null) {
+                return false;
+            } else {
+                return right.contains(value);
+            }
+        }
+    }
+
+    public void printInOrder() {
+        if (left != null) {
+            left.printInOrder();
+        }
+        System.out.println(data);
+        if (right != null) {
+            right.printInOrder();
+        }
+    }
+}
+
+public class BinarySearchTree {
+
+
+}
