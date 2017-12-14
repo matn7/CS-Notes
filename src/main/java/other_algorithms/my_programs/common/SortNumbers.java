385746e764bee0a9641ba26008ebd0c84cfe9537
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-12-14 18:39:33 +0100

    SortNumbers algorithm impl plus unit test

---
+++ src/main/java/other_algorithms/my_programs/common/SortNumbers.java
@@ -0,0 +1,40 @@
+package other_algorithms.my_programs.common;
+
+/**
+ * Created by Mati on 14.12.2017.
+ */
+public class SortNumbers {
+
+    public static void main(String[] args) {
+        int[] arr = {0,0,1,1,0,1,1,1,0,0,1,1,1};
+        System.out.println(arr.length);
+
+        int[] result = new SortNumbers().sortZeroOne(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(result[i]);
+        }
+
+    }
+
+    public int[] sortZeroOne(int[] inputArray) {
+        int[] resultArray = new int[inputArray.length];
+        int counter = 0;
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == 1) {
+                counter++;
+            }
+        }
+
+        for (int i = 0; i < inputArray.length; i++) {
+            if (i <= inputArray.length - 1 - counter) {
+                resultArray[i] = 0;
+            } else {
+                resultArray[i] = 1;
+            }
+        }
+
+        return resultArray;
+    }
+}
